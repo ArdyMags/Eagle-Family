a8b3f59d7b3fb4deecfe7f9e83e78d4c01b41e71
ArdyMags <ardymagshar27@gmail.com>
2026-06-08 22:15:03 +0700

    2-script.js

--- 2-script.js
+++ 2-script.js
@@ -770,10 +770,8 @@ function generateIuranID() {
   const MM = String(now.getMonth() + 1).padStart(2, '0');
   const dd = String(now.getDate()).padStart(2, '0');
   const HH = String(now.getHours()).padStart(2, '0');
-  const mm = String(now.getMinutes()).padStart(2, '0');
-  const ss = String(now.getSeconds()).padStart(2, '0');
   const ms = String(Math.floor(Math.random() * 1000)).padStart(3, '0');
-  return `${yyyy}.${MM}${dd}.${HH}${mm}.${ss}${ms}`;
+  return `${yyyy}${MM}${dd}${HH}${ms}`;
 }
 
 function loadBulanBayar(){
